14f97e69517ad09b6a27e3b588c2a958dabd8d36
Thomas Lewis <tplewis@gmail.com>
2019-08-19 11:44:00 -0400

    Model Updated - set to use many-to-many associations between Journal and Tags tables.  - Untried, untested

--- models/journal.js
+++ models/journal.js
@@ -1,13 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
   var Journal = sequelize.define("Journal", {
-    text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
 
-  Journal.associate = function(models) {
-    Journal.hasMany(models.Tags, {
-      // this was in the example I (TPL) used but we are not wanting to cascade deletes.
-      //onDelete: "cascade"
+  Journal.associate = (models) => {
+    Journal.belongsToMany(models.Tags, {
+      through: 'JournalTags',
+      as : 'tags',
+      foreignKey: 'JournalId'
     });
   };
   return Journal;
--- models/tags.js
+++ models/tags.js
@@ -1,28 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
   var Tags = sequelize.define("Tags", {
-    title: {
+    tagname: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1]
       }
-    },
-    body: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      len: [1]
     }
   });
 
   Tags.associate = function(models) {
     // We're saying that a Tag should belong to an Journal
     // A Tag can't be created without an Journal due to the foreign key constraint
-    Tags.belongsTo(models.Journal, {
-      foreignKey: {
-        allowNull: false
-      }
+    Tags.belongsToMany(models.Journal, {
+      through: 'JournalTags',
+      as: 'journal',
+      foreignKey: 'tagId'
     });
   };
-
   return Tags;
 };
