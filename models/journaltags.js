c0af1fc39f0472bcc6b656df6f512ad04df2fe18
Flying Serpent <nickprather11@gmail.com>
2019-08-29 18:09:49 -0400

    allEntries page now showing the most recent entry first

--- models/journaltags.js
+++ models/journaltags.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-  var JournalTag = sequelize.define("JournalTag", {
+  var JournalTag = sequelize.define("JournalTags", {
     journalId: {
       allowNull: false,
       type: DataTypes.INTEGER,
